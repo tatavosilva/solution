e2a932ca02d7cbf7c5d97ba80db3dab72b7868c8
tatavosilva <tatavosilva@users.noreply.github.com>
2019-06-14 11:02:34 -0500

    cambios de posicion de titulos

--- solution.rb
+++ solution.rb
@@ -1,9 +1,9 @@
 require 'sinatra'
 
 get '/' do
-if params[:name] == nil
-   "<h1>Hola desconocido!</h1>"
+if params[:name] != nil
+   "<h1>Hola #{params[:name]}!</h1>"
 else
-  "<h1>Hola Juan!</h1>"
+  "<h1>Hola Desconocido</h1>"
 end
 end

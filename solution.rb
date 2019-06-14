b064ff4b30e2cbae75c7dea6babf6fd360ff0349
tatavosilva <tatavosilva@users.noreply.github.com>
2019-06-14 11:18:50 -0500

    correccion de cierre de etiquetas

--- solution.rb
+++ solution.rb
@@ -1,9 +1,9 @@
 require 'sinatra'
 
 get '/' do
-if params[:nombre] != nil
-   "<h1>Hola #{params[:nombre]}!</h1>"
-else
-  "<h1>Hola desconocido!</h1>"
-end
+  if params[:nombre] != nil
+   "<h1>Hola #{params[:nombre]}!<h1>"
+  else
+  "<h1>Hola desconocido!<h1>"
+  end
 end

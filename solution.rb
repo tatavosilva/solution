b3076fbdc18d99826af5f6f7cdabaa0f5f4b9a36
tatavosilva <tatavosilva@users.noreply.github.com>
2019-06-14 11:27:09 -0500

    cambio de todo

--- solution.rb
+++ solution.rb
@@ -1,9 +1,9 @@
 require 'sinatra'
 
 get '/' do
-  if params[:nombre] != nil
-   return "<h1>Hola #{params[:nombre]}!<h1>"
-  else
+  if params[:nombre] == nil
    return "<h1>Hola desconocido!<h1>"
+  else
+   return "<h1>Hola Juan!<h1>"
   end
 end
